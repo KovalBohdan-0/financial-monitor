37affd1846774a18d8e9ed995fae0dee02330b8c
gfton <gftonot@gmail.com>
2023-05-13 14:43:16 +0300

    Init backend

---
+++ server/src/main/java/com/ftf/financialmonitor/FinancialMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ftf.financialmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMonitorApplication.class, args);
+	}
+
+}
