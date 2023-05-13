b1483ec3b62b33dbfd9734c716a84873e5851998
gfton <gftonot@gmail.com>
2023-05-13 19:08:50 +0300

    Added expense

---
+++ server/src/main/java/com/ftf/financialmonitor/expense/ExpenseRepository.java
@@ -0,0 +1,13 @@
+package com.ftf.financialmonitor.expense;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findAllByCustomerId(Long customerId);
+
+    void deleteAllByCustomerId(Long customerId);
+}
