3c0b4fa2ad8d29b3801134748d46cc4125c056c2
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-05-19 05:07:31 +0300

    completed edit functionality

--- client/src/components/EditComponent.jsx
+++ client/src/components/EditComponent.jsx
@@ -4,45 +4,119 @@ import { Stack, Typography, Container } from '@mui/material';
 import { Colors } from '../styles';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+
 function EditComponent() {
   const [activeButton, setActiveButton] = useState('Кредит');
   const [data, setData] = useState([]);
+  const [editId, setEditId] = useState('');
+  const [editValue, setEditValue] = useState('');
 
   const handleButtonClick = (button) => {
     setActiveButton(button);
   };
-  useEffect(() => {
-    async function getData() {
-      const responseData = localStorage.getItem('responseData');
-      console.log(responseData);
-      let parsedData;
 
-      if (responseData) {
-        parsedData = JSON.parse(responseData);
-        console.log(parsedData);
-      }
-      try {
-        const response = await axios.get(
-          'https://financial-monitor-production.up.railway.app/api/v1/credit/all-by-customer',
+  async function deleteBtn(id) {
+    const responseData = localStorage.getItem('responseData');
+    let parsedData;
+
+    if (responseData) {
+      parsedData = JSON.parse(responseData);
+    }
+
+    try {
+      axios
+        .delete(
+          `https://financial-monitor-production.up.railway.app/api/v1/credit/${id}`,
           {
             headers: {
               'Content-Type': 'application/json',
               Authorization: `Bearer ${parsedData}`,
             },
           }
-        );
-        setData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        if (error.response) {
-          console.log(error.response.data.message);
-        } else {
-          console.log('Error:', error.message);
+        )
+        .then((response) => {
+          console.log(response.data);
+          // Update the data state by removing the deleted item
+          setData((prevData) => prevData.filter((item) => item.id !== id));
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response.data.message);
+          } else {
+            console.log('Error:', error.message);
+          }
+        });
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  }
+
+  async function editBtn(id) {
+    const responseData = localStorage.getItem('responseData');
+    let parsedData;
+
+    if (responseData) {
+      parsedData = JSON.parse(responseData);
+    }
+
+    try {
+      await axios.put(
+        `https://financial-monitor-production.up.railway.app/api/v1/credit/${id}`,
+        { money: editValue },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${parsedData}`,
+          },
         }
+      );
+
+      // Clear the edit fields
+      setEditId('');
+      setEditValue('');
+      // Fetch updated data
+      getData();
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+      } else {
+        console.log('Error:', error.message);
       }
     }
+  }
+
+  async function getData() {
+    const responseData = localStorage.getItem('responseData');
+    let parsedData;
+
+    if (responseData) {
+      parsedData = JSON.parse(responseData);
+    }
+
+    try {
+      const response = await axios.get(
+        'https://financial-monitor-production.up.railway.app/api/v1/credit/all-by-customer',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${parsedData}`,
+          },
+        }
+      );
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+      } else {
+        console.log('Error:', error.message);
+      }
+    }
+  }
+
+  useEffect(() => {
     getData();
-  }, [activeButton]);
+  }, []);
 
   return (
     <>
@@ -118,7 +192,27 @@ function EditComponent() {
         </Box>
         <Box>
           {data.map((item) => {
-            return <div key={item.id}>{item.money}</div>;
+            return (
+              <div key={item.id}>
+                {editId === item.id ? (
+                  <>
+                    <input
+                      type='text'
+                      value={editValue}
+                      onChange={(e) => setEditValue(e.target.value)}
+                    />
+                    <button onClick={() => editBtn(item.id)}>save</button>
+                    <button onClick={() => setEditId('')}>cancel</button>
+                  </>
+                ) : (
+                  <>
+                    {item.money}
+                    <button onClick={() => deleteBtn(item.id)}>delete</button>
+                    <button onClick={() => setEditId(item.id)}>edit</button>
+                  </>
+                )}
+              </div>
+            );
           })}
         </Box>
       </Box>
